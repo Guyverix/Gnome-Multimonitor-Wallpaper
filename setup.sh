417c46fc583016cccca19fea23592917b7c82404
Chris Hubbard <guyverix@yahoo.com>
2022-11-03 12:17:09 -0700

    update the installer

--- setup.sh
+++ setup.sh
@@ -14,47 +14,45 @@ else
   echo "root"
 fi
 }
-canonicalpath=`readlink -f $0`
-location=`dirname "${canonicalpath}"`
 
-if [ ! "${location}" == "/home/${USER}/.multi_wall" ];then
-  if [ ! -e "~/.multi_wall" ];then
-    mkdir ~/.multi_wall
-  fi
-  cp -R * ~/.multi_wall/
-else
-  echo "It appears that this script is already running in .multi_wall"
-fi
+# Set default
 
 USERID=`got_root`
 if [ "${USERID}" == "root" ];then
-  echo "While sudo is required, this should be run as yourself (so we can find your home directory)"
+  echo "sudo is not required, this should be run as yourself (so we can find your home directory)"
   exit 1
 else
   echo "Confirmed we are not running as root user... Continuing"; sleep .5
 fi
 
-# Setup should not need a lot of brains, and we do NOT want to screw with the users machine and possibly break things.
+if [ ! -e "~/.multi_wall" ];then
+  mkdir ~/.multi_wall
+  cp -R * ~/.multi_wall/
+else
+  echo "The .multi_wall directory already exists"
+  echo "Not taking the chance of clobbering any customizing done"
+fi
+
+# Setup should not need a lot of brains, and we do NOT want to
+# screw with the users machine and possibly break things.
 # Keep it simple!
 
-# Make a common path.  This will change later after code consolidation happens.  Those changes will not hurt things.
-# So even though this is not a good idea (what happens with more than one user?) this will not damage the machine.
-if [ ! -e /usr/local/bin/wallpapers ];then
-  echo "Creating symlink wallpapers in /usr/local/bin"
-  sudo ln -s /home/${USER}/.multi_wall/multi_wallpapers.sh /usr/local/bin/wallpapers
-else
-  echo "Symlink /usr/local/bin/wallpapers already exists."
-fi
-sleep .5
-
 # Assume that the user who is running setup will be the only one
+# This will not damage other users of the system
 if [ ! -e /home/${USER}/.multi_wall/images.lst ];then
   echo "Configure  /home/${USER}/.multi_wall/multi.cfg with the needed path for images and monitor resolutions"
   echo "See the README in the .multi_wall directory for details.  Additional examples are included in the directory"
+  echo "or on github."
 else
   echo "The images.lst file exists.  This is likely an update."
+  echo "The file copy needs to be done manually so we do not clobber"
+  echo "files such as images.lst and multi.cfg"
 fi
 sleep .5
+
+# Here is where we add to the desktop autorun (if we can figure out how)
+cat multiwallpaper.desktop | sed "s/USERID/${USERID}/g" > ~/.config/autostart/multiwallpaper.desktop
+echo "Application is set to start on login"
 
 echo "Setup complete"; exit 0
 
